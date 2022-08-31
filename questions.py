85c2bd48227a13d7887820ee7df2536ea1ddde1c
Bogdandev <jobsnospam007@gmail.com>
2022-08-31 02:39:17 +0000

    refactoring  created file with questions in list of dictionaries

---
+++ questions.py
@@ -0,0 +1,57 @@
+question_list = [{
+              "question": "Which group is Bentley a subsidiary of? ",
+              "options" : ["A. Volkswagen Group", "B. PSA", "C. Ford Motor Company", "D. Hyundai Motor Group"],
+              "correctOption": "A"
+              },
+             
+             {
+              "question": "What does BMW stand for? ",
+              "options" : ["A. Berlin Motor Works", "B. Brunswick Motor Works", "C. Bavarian Motor Works", "D. Borgoholzhausen Motor Works"],
+              "correctOption": "C"
+              },
+             
+             {
+              "question": "Which 2020 Audi model has the highest starting MSRP? ",
+              "options" : ["A. Q7", "B. S6", "C. TTS", "D. A8"],
+              "correctOption": "D"  
+             },
+             {
+              "question": "In which city can you find the headquarters of Kia? ",
+              "options" : ["A. Bangkok", "B. Kuala Lumpur", "C. Singapore ", "D. Seoul "],
+              "correctOption": "A"
+              },
+
+              {
+              "question": "What is the best selling car of all time? ",
+              "options" : ["A. Peugeot 206", "B. Volkswagen Golf", "C. Fiat 500", "D. Toyota Corolla"],
+              "correctOption": "D"
+              },
+
+              {
+              "question": "What is Ford’s slogan? ",
+              "options" : ["A. Motion & Emotion", "B. Go Further", "C. Above & Beyond", "D. Do Anything"],
+              "correctOption": "B"
+              },
+
+              {
+              "question": "Which colour is Mr Bean’s car? ",
+              "options" : ["A. Red", "B. Blue", "C. Green", "D. Yellow"],
+              "correctOption": "C"
+              },
+              {
+              "question": "Which car company uses the tagline 'The ultimate driving machine'? ",
+              "options" : ["A. BMW", "B. Audi", "C. Toyota", "D. Ford"],
+              "correctOption": "A"
+              },
+              {
+              "question": "Who is known for inventing the gas-powered internal combustion engine? ",
+              "options" : ["A. Karl Benz", "B. Henry Ford", "C. Nicolaus Otto", "D. Frankenstein"],
+              "correctOption": "C"
+              },
+              {
+              "question": "In the logo of Audi, how many rings are there? ",
+              "options" : ["A. One", "B. Four", "C. Seven", "D. No rings there"],
+              "correctOption": "B"
+              }
+             
+             ]
