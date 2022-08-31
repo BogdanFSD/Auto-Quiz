13489e2ba64851161a2a77d312874beeda48e212
Bogdandev <jobsnospam007@gmail.com>
2022-08-31 02:10:38 +0000

    Initial commit

--- run.py
+++ run.py
@@ -1,3 +1,14 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+def greeting_screen():
+    pass
+
+def start_game():
+    pass
+
+def correct_answers():
+    pass
+
+def show_points():
+    pass
+
+def restart():
+    pass
