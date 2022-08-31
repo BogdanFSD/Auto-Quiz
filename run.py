e67cda33cb0058a5cd831b2bb1e398d5a6e2ffad
Bogdandev <jobsnospam007@gmail.com>
2022-08-31 03:12:16 +0000

    finished loop for question left

--- run.py
+++ run.py
@@ -9,8 +9,6 @@ colorama.init(autoreset=True)
 
 
 
-
-
 def prepare_quiz_questions():
     """
    Function that will randomize order of our
@@ -24,10 +22,10 @@ class Game:
     That helps programm runs properly
     and display everything in correct order
     """
-    def __init__(self, que_list):
-        self.question_number = 0
+    def __init__(self, q_list):
+        self.q_number = 0
         self.score = 0
-        self.que_list = que_list
+        self.que_list = q_list
 
     def n_question(self):
         """
@@ -53,146 +51,43 @@ class Game:
                 print('Invalid entry. Please try again ')
                 print()
 
-        def question_left(self):
+        self.validation_a(user_answer, following_q['correctOption'])
+
+    def question_left(self):
         """
         checks if any questions left after printed one
         """
         # returns true if we have another question
         return self.q_number < len(self.que_list)
 
+    def validation_a(self, user_answer, answer):
+        """
+        Compares answer of the user and correct answer to
+        the question
+        """
+        if user_answer.upper() == answer.upper():
+            self.score += 1  # Once correct increment score
+            print()
+            print(Fore.LIGHTGREEN_EX+'corect')
+            print()
+        elif user_answer.upper() != answer.upper():
+            self.score += 0  # once worn prints statement and add 0 to score
+            print()
+            print(Fore.LIGHTRED_EX+'wrong')
+            print()
+            print(f"correct is {answer}")  # shwos correct answer
+            print()
+        # shows score overall till that question included
+        print(f"Current score is {self.score} out of 10")
+        print()
+
+
+prepare_quiz_questions()
+game = Game(question_list)
+
+while game.question_left():
+    game.n_question()
 
 
 
 
-
-
-
-
-"""def greeting_screen():
-    print("Welcome to a Fun Car Quiz! \nAre you ready to check your knowledge?")
-    print("There are total of 4 question and 4 answers to each of them. Once you are done with all question you will receive your score.")
-    print(r"""
-                                                ______--------___
-                                        /|             / |
-                            o___________|_\__________/__|
-                            ]|___     |  |=   ||  =|___  |"
-                            //   \\    |  |____||_///   \\|"
-                            |  X  |\--------------/|  X  |\"
-                            \___/       1924        \___/
-        """)
-
-
-    while True:
-        start = input("Are you ready for a rummmmmmble? (Yes/No) ")
-
-        if start.lower() == "yes":
-            print("great")
-            start_game()
-            return True
-
-        elif start.lower() == "no":
-            print("BYEEE")
-            quit()
-
-        else:
-            print("Invalid entry. Yes or No")
-
-            continue
-
-    start_game()
-
-def start_game():
-    """
-    function that runs all necessary fnuction to start
-    """
-    attempts = []
-    correct_attempts = 0
-    question_num = 0 # index of questions to help me itarate 
-
-    for question in questions:  #
-        print("*" * 10) # just to make it more pleasant to for human eye
-        print(question)
-        for choice in choices[question_num]: 
-            print(choice) 
-
-        while True:                    
-            attempt = input("Choose an answer A, B, C or D: ").upper()
-            if attempt in {"A", "B", "C", "D"}:
-                attempts.append(attempt)
-                break
-            else:
-                print("Invalid entry")
-
-        correct_attempts += correct_answers(questions.get(question), attempt)
-        question_num += 1
-
-    show_points(correct_attempts, attempts)
-
-
-def correct_answers(answer, attempt):
-    if answer == attempt:
-        print("corect")
-        return 1
-    else:
-        print("wrong")
-        return 0
-
-def show_points(correct_attempts, attempts):
-    print("*" * 10)
-    print("result")
-    print("Answers: ")
-    for i in questions:
-        print(questions.get(i))
-    print()
-
-    print("Attempts")
-    for i in attempts:
-        print(i)
-    print()
-
-    score = int((correct_attempts/len(questions))*100)
-    print("your score is " + str(score)+ "%")
-
-    restart()
-
-def restart():
-    while True:
-        response = input("Do you want to continue? (Yes/No) ")
-
-        if response.lower() == "yes":
-            print("great")
-            start_game()
-            return True
-
-        elif response.lower() == "no":
-            print("BYEEE")
-            quit()
-
-        else:
-            print("Invalid entry. Yes or No")
-
-            continue
-
-
-
-
-questions = {
-    "Who Created tesla?": "A",
-    "What does BMW stand for?": "C",
-    "Which 2020 Audi model has the highest starting MSRP?": "D",
-    "Which group is Bentley a subsidiary of?": "A",
-    "In which city can you find the headquarters of Kia?": "A"
-}
-
-choices = [
-    ["A. Elon Musk", "B. Bill Gates", "C. Ford", "D. Johny Depp"],
-    ["A. Berlin Motor Works", "B. Brunswick Motor Works","C. Bavarian Motor Works", "D. Borgoholzhausen Motor Works"],
-    ["A. Q7", "B. S6", "C. TTS", "D. A8"],
-    ["A. Volkswagen Group", "B. PSA", "C. Ford Motor Company", "D. Hyundai Motor Group"],
-    ["A. Bangkok", "B. Kuala Lumpur", "C. Singapore ", "D. Seoul "]
-    ]
-
-
-if __name__ == "__main__":
-    greeting_screen()
-"""
